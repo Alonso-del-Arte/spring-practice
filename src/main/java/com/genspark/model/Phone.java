12225092b5e2dbbddcd66576df3f728ec02dba6a
Alonso del Arte <alonso.delarte@gmail.com>
2022-07-20 15:42:56 -0400

    IntelliJ-generated getters and setters

--- src/main/java/com/genspark/model/Phone.java
+++ src/main/java/com/genspark/model/Phone.java
@@ -4,4 +4,11 @@ public class Phone {
 
     private String mobile;
 
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 }
--- src/main/java/com/genspark/model/Student.java
+++ src/main/java/com/genspark/model/Student.java
@@ -9,4 +9,36 @@ public class Student {
     private List<Phone> phones;
     private Address address;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
 }
