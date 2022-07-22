fc5a5e29019efee4dffe06db197a6cc2e7d37496
Alonso del Arte <alonso.delarte@gmail.com>
2022-07-22 09:37:50 -0400

    Adding psvm to AppConfig

--- src/main/java/com/genspark/AppConfig.java
+++ src/main/java/com/genspark/AppConfig.java
@@ -1,9 +1,21 @@
 package com.genspark;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation
+        .AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "com.genspark.model")
 public class AppConfig {
+
+    public static void main(String[] args) {
+        ApplicationContext appContext
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        for (String beanName : appContext.getBeanDefinitionNames()) {
+            System.out.println(beanName);
+        }
+    }
+
 }
