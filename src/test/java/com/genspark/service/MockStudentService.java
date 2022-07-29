820f4eec66801feebcfee7acd2e2bcdf471750bd
Alonso del Arte <alonso.delarte@gmail.com>
2022-07-29 09:47:11 -0400

    Passing all tests so far

--- src/test/java/com/genspark/service/MockStudentService.java
+++ src/test/java/com/genspark/service/MockStudentService.java
@@ -3,27 +3,30 @@ package com.genspark.service;
 import com.genspark.entity.Student;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MockStudentService implements StudentService {
 
-    private final List<Student> students = new ArrayList<>();
+    private final Map<Integer, Student> students = new HashMap<>();
 
     @Override
     public List<Student> getAllStudents() {
-        return new ArrayList<>(this.students);
+        return new ArrayList<>(this.students.values());
     }
 
-    // TODO: Write tests for this
+    // TODO: Write tests for what result should be when id not found
     @Override
     public Student getStudentById(int id) {
-        return null;
+        return this.students.getOrDefault(id, new Student());
     }
 
-    // TODO: Write tests for this
     @Override
     public Student addStudent(Student student) {
-        this.students.add(student);
+        int id = this.students.size() + 1;
+        student.setId(id);
+        this.students.put(id, student);
         return student;
     }
 
--- src/test/java/com/genspark/service/MockStudentServiceTest.java
+++ src/test/java/com/genspark/service/MockStudentServiceTest.java
@@ -2,12 +2,20 @@ package com.genspark.service;
 
 import com.genspark.entity.Student;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Tests of the MockStudentService class.
+ * @author Alonso del Arte
+ */
 public class MockStudentServiceTest {
 
     private static final Random RANDOM = new Random();
@@ -37,18 +45,59 @@ public class MockStudentServiceTest {
         assertEquals(expected, actual, msg);
     }
 
+    @Test
+    void testAddStudent() {
+        System.out.println("addStudent");
+        Student student = new Student();
+        student.setId(Integer.MIN_VALUE);
+        String name = "John Q. Test";
+        student.setName(name);
+        StudentService mock = new MockStudentService();
+        mock.addStudent(student);
+        List<Student> list = mock.getAllStudents();
+        String msg = "After adding " + name
+                + " to mock, student's name should be in mock";
+        assert list.stream().anyMatch(s -> s.getName().equals(name)) : msg;
+    }
 
+    @Test
+    void testAddStudentGivesUniqueIDNumbers() {
+        int expected = RANDOM.nextInt(128) + 32;
+        StudentService mock = new MockStudentService();
+        for (int i = 0; i < expected; i++) {
+            Student student = new Student();
+            student.setId(Integer.MAX_VALUE);
+            student.setName("Test Student " + i);
+            mock.addStudent(student);
+        }
+        List<Student> students = mock.getAllStudents();
+        Set<Integer> idNumbers = new HashSet<>(expected);
+        for (Student student : students) {
+            idNumbers.add(student.getId());
+        }
+        int actual = idNumbers.size();
+        String msg = "Given " + expected
+                + " students, there should be as many ID numbers";
+        assertEquals(expected, actual, msg);
+    }
 
-
-
-
-
-
-
-
-
-
-
-
+    @Test
+    void testGetStudentById() {
+        System.out.println("getStudentById");
+        int capacity = RANDOM.nextInt(128) + 32;
+        StudentService mock = new MockStudentService();
+        Map<Integer, Student> map = new HashMap<>(capacity);
+        for (int i = 0; i < capacity; i++) {
+            Student student = new Student();
+            student.setName("Test Student " + i);
+            Student addedStudent = mock.addStudent(student);
+            map.put(addedStudent.getId(), addedStudent);
+        }
+        for (int id : map.keySet()) {
+            Student expected = map.get(id);
+            Student actual = mock.getStudentById(id);
+            assertEquals(expected, actual);
+        }
+    }
 
 }
